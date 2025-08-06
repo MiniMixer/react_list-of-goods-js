e790a36a0d93f6f35073c898b8860c64c67047a2
Radomur Vlasuk <radical123boss@gmail.com>
2025-08-06 20:05:55 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function getPreparedGoods(goods, { sortField }) {
         case SORT_FIELD_ALPHABETICALLY:
           return good1.localeCompare(good2);
         case SORT_FIELD_LENGTH:
-          return good1[sortField] - good2[sortField];
+          return good1.length - good2.length;
 
         default:
           return visibleGoods;
@@ -87,7 +87,7 @@ export const App = () => {
             type="button"
             className="button is-danger is-light"
             onClick={() => {
-              setSortField();
+              setSortField('');
               setReverse(false);
             }}
           >
